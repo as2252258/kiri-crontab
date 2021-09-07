ac46ab9f1bc87d4d08f840dec7fb2d5a13cfd044
Mr·x <as2252258@163.com>
2021-09-07 14:54:23 +0800

    改名

--- src/Crontab.php
+++ src/Crontab.php
@@ -7,7 +7,9 @@ namespace Kiri\Crontab;
 use Exception;
 use JetBrains\PhpStorm\Pure;
 use Kiri\Application;
+use Kiri\Exception\NotFindClassException;
 use Kiri\Kiri;
+use ReflectionException;
 
 /**
  * Class Async
@@ -17,172 +19,174 @@ use Kiri\Kiri;
 abstract class Crontab implements CrontabInterface
 {
 
-    const WAIT_END = 'crontab:wait:execute';
+	const WAIT_END = 'crontab:wait:execute';
 
 
-    public string $name = '';
+	public string $name = '';
 
 
-    public mixed $params;
+	public mixed $params;
 
 
-    public int $tickTime;
+	public int $tickTime;
 
 
-    public bool $isLoop;
+	public bool $isLoop;
 
 
-    public int $timerId = -1;
+	public int $timerId = -1;
 
 
-    /**
-     * Crontab constructor.
-     * @param mixed $params
-     * @param false $isLoop
-     * @param int $tickTime
-     */
-    public function __construct(mixed $params, bool $isLoop = false, int $tickTime = 1)
-    {
-        $this->params = $params;
-        $this->isLoop = $isLoop;
-        $this->tickTime = $tickTime;
-    }
+	/**
+	 * Crontab constructor.
+	 * @param mixed $params
+	 * @param false $isLoop
+	 * @param int $tickTime
+	 */
+	public function __construct(mixed $params, bool $isLoop = false, int $tickTime = 1)
+	{
+		$this->params = $params;
+		$this->isLoop = $isLoop;
+		$this->tickTime = $tickTime;
+	}
 
 
-    /**
-     * @return Application
-     */
-    #[Pure] private function getApplication(): Application
-    {
-        return Kiri::app();
-    }
+	/**
+	 * @return Application
+	 */
+	#[Pure] private function getApplication(): Application
+	{
+		return Kiri::app();
+	}
 
 
-    /**
-     * @return string
-     */
-    #[Pure] public function getName(): string
-    {
-        return md5($this->name);
-    }
+	/**
+	 * @return string
+	 */
+	#[Pure] public function getName(): string
+	{
+		return md5($this->name);
+	}
 
-    /**
-     * @return mixed
-     */
-    public function getParams(): mixed
-    {
-        return $this->params;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getParams(): mixed
+	{
+		return $this->params;
+	}
 
-    /**
-     * @return int
-     */
-    public function getTickTime(): int
-    {
-        return $this->tickTime;
-    }
+	/**
+	 * @return int
+	 */
+	public function getTickTime(): int
+	{
+		return $this->tickTime;
+	}
 
-    /**
-     * @return bool
-     */
-    public function isLoop(): bool
-    {
-        return $this->isLoop;
-    }
-
-
-    /**
-     * @param string $name
-     */
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
+	/**
+	 * @return bool
+	 */
+	public function isLoop(): bool
+	{
+		return $this->isLoop;
+	}
 
 
-    /**
-     * @return int
-     */
-    public function getTimerId(): int
-    {
-        return $this->timerId;
-    }
+	/**
+	 * @param string $name
+	 */
+	public function setName(string $name): void
+	{
+		$this->name = $name;
+	}
 
 
-    /**
-     * @param $name
-     * @return mixed
-     */
-    #[Pure] public function __get($name): mixed
-    {
-        if ($name === 'application') {
-            return $this->getApplication();
-        }
-        if (!isset($this->params[$name])) {
-            return null;
-        }
-        return $this->params[$name];
-    }
+	/**
+	 * @return int
+	 */
+	public function getTimerId(): int
+	{
+		return $this->timerId;
+	}
 
 
-    /**
-     * @throws Exception
-     */
-    public function process(): void
-    {
-        try {
-            $redis = $this->application->getRedis();
-            $name_md5 = $this->getName();
-
-            defer(fn() => $this->onRecover($name_md5));
-
-            call_user_func([$this, 'execute']);
-
-            $redis->hDel(self::WAIT_END, $name_md5);
-        } catch (\Throwable $throwable) {
-            $this->application->addError($throwable, 'throwable');
-        }
-    }
+	/**
+	 * @param $name
+	 * @return mixed
+	 */
+	#[Pure] public function __get($name): mixed
+	{
+		if ($name === 'application') {
+			return $this->getApplication();
+		}
+		if (!isset($this->params[$name])) {
+			return null;
+		}
+		return $this->params[$name];
+	}
 
 
-    /**
-     * @param $name_md5
-     * @throws \Kiri\Exception\NotFindClassException
-     * @throws \ReflectionException
-     * @throws \Exception
-     */
-    protected function onRecover($name_md5)
-    {
-        $redis = $this->application->getRedis();
-        /** @var \Kiri\Crontab\Producer $crontab */
-        $crontab = Kiri::getFactory()->get('crontab');
-        if ($redis->sIsMember(Producer::CRONTAB_STOP_KEY, $name_md5)) {
-            return $redis->sRem(Producer::CRONTAB_STOP_KEY, $name_md5);
-        }
-        if ($this->isPropagationStopped()) {
-            return true;
-        }
-        return $crontab->task($this);
-    }
+	/**
+	 * @throws Exception
+	 */
+	public function process(): void
+	{
+		try {
+			$redis = $this->application->getRedis();
+
+			$name_md5 = $this->getName();
+
+			call_user_func([$this, 'execute']);
+
+			$redis->hDel(self::WAIT_END, $name_md5);
+		} catch (\Throwable $throwable) {
+			$this->application->addError($throwable, 'throwable');
+		} finally {
+			$this->onRecover($this->getName());
+		}
+	}
 
 
-    /**
-     * @return int
-     */
-    private function next(): int
-    {
-        return time() + $this->getTickTime();
-    }
+	/**
+	 * @param $name_md5
+	 * @return bool|int
+	 * @throws NotFindClassException
+	 * @throws ReflectionException
+	 * @throws Exception
+	 */
+	protected function onRecover($name_md5): bool|int
+	{
+		$redis = $this->application->getRedis();
+		/** @var Producer $crontab */
+		$crontab = Kiri::getFactory()->get('crontab');
+		if ($redis->sIsMember(Producer::CRONTAB_STOP_KEY, $name_md5)) {
+			return $redis->sRem(Producer::CRONTAB_STOP_KEY, $name_md5);
+		}
+		if ($this->isPropagationStopped()) {
+			return true;
+		}
+		return $crontab->task($this);
+	}
 
 
-    /**
-     * @param $class
-     * @return string
-     */
-    public static function serialize($class): string
-    {
-        return serialize($class);
-    }
+	/**
+	 * @return int
+	 */
+	private function next(): int
+	{
+		return time() + $this->getTickTime();
+	}
+
+
+	/**
+	 * @param $class
+	 * @return string
+	 */
+	public static function serialize($class): string
+	{
+		return serialize($class);
+	}
 
 
 }
