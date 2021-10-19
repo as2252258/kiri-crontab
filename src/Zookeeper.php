0b19d3afc95e6fa599bb63590ce72880406a767d
向林 <as2252258@163.com>
2021-10-19 16:23:55 +0800

    改名

--- src/Zookeeper.php
+++ src/Zookeeper.php
@@ -10,7 +10,7 @@ use Kiri\Cache\Redis;
 use Kiri\Error\Logger;
 use Kiri\Exception\ConfigException;
 use Kiri\Kiri;
-use Server\Abstracts\OnProcessInterface;
+use Server\Abstracts\BaseProcess;
 use Server\ServerManager;
 use Swoole\Process;
 use Swoole\Timer;
@@ -20,7 +20,7 @@ use Throwable;
  * Class Zookeeper
  * @package Kiri\Process
  */
-class Zookeeper extends OnProcessInterface
+class Zookeeper extends BaseProcess
 {
 
 
