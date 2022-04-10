72182d85efa26a53c2365e8af068b407dad04788
as2252258 <as2252258@163.com>
2022-04-10 14:59:28 +0800

    modify mysql result

--- Zookeeper.php
+++ Zookeeper.php
@@ -10,6 +10,7 @@ use Kiri\Redis\Redis;
 use Kiri\Server\Abstracts\BaseProcess;
 use Kiri\Server\Broadcast\OnBroadcastInterface;
 use Psr\Log\LoggerInterface;
+use Swoole\Coroutine;
 use Swoole\Process;
 use Swoole\Timer;
 use Throwable;
@@ -22,49 +23,61 @@ class Zookeeper extends BaseProcess
 {
 
 
-	public string $name = 'crontab zookeeper';
+    public string $name = 'crontab zookeeper';
 
 
-	protected bool $enable_coroutine = true;
+    protected bool $enable_coroutine = true;
 
 
-	public ?int $timerId = null;
+    public ?int $timerId = null;
 
 
-	/**
-	 * @param Process $process
-	 * @throws Exception
-	 */
-	public function process(Process $process): void
-	{
-		$logger = Kiri::getDi()->get(LoggerInterface::class);
-		$this->timerId = Timer::tick(100, [$this, 'loop'], $logger);
-	}
+    /**
+     * @param Process $process
+     * @throws Exception
+     */
+    public function process(Process $process): void
+    {
+        $logger = Kiri::getDi()->get(LoggerInterface::class);
+        $this->timerId = Timer::tick(100, [$this, 'loop'], $logger);
+    }
 
 
-	/**
-	 * @param OnBroadcastInterface $message
-	 * @return void
-	 */
-	public function onBroadcast(OnBroadcastInterface $message): void
-	{
-		$logger = Kiri::getDi()->get(LoggerInterface::class);
-		$logger->debug($message->data . '::' . static::class);
-	}
+    /**
+     * @param OnBroadcastInterface $message
+     * @return void
+     */
+    public function onBroadcast(OnBroadcastInterface $message): void
+    {
+        $logger = Kiri::getDi()->get(LoggerInterface::class);
+        $logger->debug($message->data . '::' . static::class);
+    }
 
 
-	/**
-	 * @throws Exception
-	 */
-	public function loop($timerId, $logger)
-	{
-		if ($this->isStop()) {
+    public function onSigterm(): static
+    {
+        Coroutine::create(function () {
+            $data = Coroutine::waitSignal(SIGTERM, -1);
+            if ($data) {
+                $this->isStop = true;
+            }
+        });
+        return $this;
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    public function loop($timerId, $logger)
+    {
+        if ($this->isStop()) {
             Timer::clear($timerId);
             return;
-		}
-		$redis = Kiri::getDi()->get(Redis::class);
+        }
+        $redis = Kiri::getDi()->get(Redis::class);
 
-		$script = <<<SCRIPT
+        $script = <<<SCRIPT
 local _two = redis.call('zRangeByScore', KEYS[1], '0', ARGV[1])
 
 if (table.getn(_two) > 0) then
@@ -73,31 +86,31 @@ end
 
 return _two
 SCRIPT;
-		$range = $redis->eval($script, [Producer::CRONTAB_KEY, (string)time()], 1);
-		foreach ($range as $value) {
-			try {
-				$handler = $redis->get(Producer::CRONTAB_PREFIX . $value);
-				$redis->del(Producer::CRONTAB_PREFIX . $value);
-				if (!empty($handler)) {
-					$this->execute($handler);
-				}
-			} catch (Throwable $exception) {
-				$logger->addError($exception);
-			}
-		}
-	}
+        $range = $redis->eval($script, [Producer::CRONTAB_KEY, (string)time()], 1);
+        foreach ($range as $value) {
+            try {
+                $handler = $redis->get(Producer::CRONTAB_PREFIX . $value);
+                $redis->del(Producer::CRONTAB_PREFIX . $value);
+                if (!empty($handler)) {
+                    $this->execute($handler);
+                }
+            } catch (Throwable $exception) {
+                $logger->addError($exception);
+            }
+        }
+    }
 
 
-	private function execute($handler)
-	{
-		go(function () use ($handler) {
-			$serialize = swoole_unserialize($handler);
-			if (is_null($serialize)) {
-				return;
-			}
-			$serialize->process();
-		});
-	}
+    private function execute($handler)
+    {
+        go(function () use ($handler) {
+            $serialize = swoole_unserialize($handler);
+            if (is_null($serialize)) {
+                return;
+            }
+            $serialize->process();
+        });
+    }
 
 
 }
